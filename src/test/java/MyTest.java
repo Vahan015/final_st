2b0cd90076b79489a6a6fccc24376357dae4c583
Vahan <vahan_babayan@edu.aua.am>
2023-05-20 04:32:24 +0400

    feat: add record failure

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,7 +1,14 @@
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -27,6 +34,19 @@ public class MyTest {
     @BeforeMethod
     public void refresh(){
         driver.get(baseURL);
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try{
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Test
